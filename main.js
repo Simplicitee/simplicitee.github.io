b43b2b62c324f7c9efac6eac2872aebf213594ab
Simplicitee <simplicitee123@gmail.com>
2016-10-07 13:03:52 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 main = function() {
   var percent = document.getElementById("bar").style.width;
   if (percent === 100%) {
-    document.getElementById("bar").style.width=0%;
+    document.getElementById("bar").style.width = "0%";
   } else {
     var p = parseFloat(percent) + 1;
     p += "%";
-    document.getElementById("bar").style.width=p;
+    document.getElementById("bar").style.width = p;
   }
 };
 
