1e1df882845c9c389266310c0988745386f36e39
Simplicitee <simplicitee123@gmail.com>
2016-10-07 12:56:39 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
-
+main = function() {
+  var percent = document.getElementById("bar").style.width;
+  if (percent === 100%) {
+    document.getElementById("bar").style.width=0%;
+  } else {
+    var p = percent+1%;
+    document.getElementById("bar").style.width=p;
+  }
+};
 
 document.ready(main());
