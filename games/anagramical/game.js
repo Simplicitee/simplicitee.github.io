8d7cec8a530ceb08d49133e4b68064ac0102af67
Simplicitee <simplicitee123@gmail.com>
2017-02-10 12:32:58 -0500

    Update game.js

--- games/anagramical/game.js
+++ games/anagramical/game.js
@@ -1,17 +1,16 @@
 var easy = [{anagram: "gdo", word: "dog"}, {anagram: "pepla", word: "apple"}];
 var hard = [{anagram: "niofsucno", word: "confusion"}, {anagram: "tecabusfo", word: "obfuscate"}];
-Random r = new Random();
 
 newAnagram = function(var number) {
   var rand;
   var word;
   var correct;
   if (number === 1) {
-    rand = r.nextInt(easy.size());
+    rand = 1;
     word = easy.indexOf(rand).anagram;
     correct = easy.indexOf(rand).word;
   } else if (number === 2) {
-    rand = r.nextInt(easy.size());
+    rand = 1;
     word = hard.indexOf(rand).anagram;
     correct = hard.indexOf(rand).word;
   }
@@ -32,6 +31,6 @@ calculateAccuracy = function(var input, var word, var scramble) {
 };
 
 addPoint = function() {
-  var val = $(".counter").text();
-  $(".counter").text(val + 1);
+  var val = $("points").text();
+  $("points").text(val + 1);
 };
